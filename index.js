1a2a4dcb30d57d55957d06d41ca3c4428a0ac10a
8thTankBattalionHLL <8thtankbattalionhll@gmail.com>
2025-02-13 19:34:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Wtaching The 8th Tank Battalion", "Listening To HQ"];
+const statusMessages = ["Watching The 8th Tank Battalion", "Listening To Army HQ"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
